5ddc2a471a3b22baa8e22f3f15ccfa5edd9c9a45
noahm <noah.matlik@gmail.com>
2023-12-10 16:26:29 -0500

    optimized tree builder's data structure

--- gametrees/build_tree.py
+++ gametrees/build_tree.py
@@ -1,5 +1,6 @@
 import multiprocessing
 import copy
+from square_matrix import SquareMatrix
 
 class Node:
     def __init__(self, gamestate):
@@ -14,15 +15,15 @@ class Node:
 
     def __str__(self):
         return str(self.state) + " " + str(len(self.children))
+    
+    def get_total_nodes(self):
+        totalNodes = 0
+        for child in self.children:
+            totalNodes += child.get_total_nodes()
+        return totalNodes + 1
 
 def build_base_state(edgeSize):
-    baseState = []
-    row = []
-    for i in range(edgeSize):
-        row.append(0)
-    for i in range(edgeSize):
-        baseState.append(row.copy())
-    return baseState
+    return SquareMatrix(edgeSize)
 
 def multiprocessed_tree(n):
     state = build_base_state(n)
@@ -30,25 +31,22 @@ def multiprocessed_tree(n):
 
     resultQueue = multiprocessing.Queue()
 
-    for i in range(len(state)):
-        for j in range(len(state[i])):
-            # these states will never appear in gameplay, as the bot never goes first
-            # posState = copy.deepcopy(state)
-            # posState[i][j] = 1
-            # processes.append(multiprocessing.Process(target=parallel_funct, args=(posState, resultQueue)))
+    for i in range(state.get_size()):
+        for j in range(state.get_size()):
             negState = copy.deepcopy(state)
-            negState[i][j] = -1
+            # -1 user always goes first
+            negState.set_value(i, j, -1)
             processes.append(multiprocessing.Process(target=parallel_funct, args=(negState, resultQueue)))
     
-    # start threads
+    # start processes
     for i, process in enumerate(processes):
-        print("starting thread " + str(i+1) + "...")
+        print("starting process " + str(i+1) + "...")
         process.start()
 
-    # append results from each thread as they are generated
+    # append results from each process as they are generated
     results = [resultQueue.get() for process in processes]
 
-    # ensure function doesn't return before all threads are complete
+    # ensure function doesn't return before all processes are complete
     for i, process in enumerate(processes):
         process.join()
 
@@ -61,28 +59,32 @@ def multiprocessed_tree(n):
 
 def parallel_funct(state, rootNodeChildren):
     print("generating subtree from state " + str(state))
-    rootNodeChildren.put(create_node(state))
+    rootNodeChildren.put(create_node(state, True))
 
-def create_node(state):
+#TODO: would help performance
+def has_win_state(state):
+    return False
+
+def create_node(state, isBotTurn):
     newNode = Node(state)
-    for i in range(len(state)):
-        for j in range(len(state[i])):
-            if state[i][j] == 0:
+    for i in range(state.get_size()):
+        for j in range(state.get_size()):
+            if state.get_value(i, j) == 0:
                 nodeOne = copy.deepcopy(state)
-                nodeOne[i][j]= 1
-                oneNode = create_node(nodeOne)
-                nodeNegOne = copy.deepcopy(state)
-                nodeNegOne[i][j]= -1
-                negOneNode = create_node(nodeNegOne)
+                if isBotTurn:
+                    nodeOne.set_value(i, j, 1)
+                else:
+                    nodeOne.set_value(i, j, -1)
+                if not has_win_state(newNode.state):
+                    oneNode = create_node(nodeOne, not isBotTurn)
                 if oneNode != None:
                     newNode.add_child(oneNode)
-                if negOneNode != None:
-                    newNode.add_child(negOneNode)
-    #print(newNode.state, str(len(newNode.children)) + " children")
+    # print(newNode.state, str(len(newNode.children)) + " children")
     return newNode
 
-# I wouldn't reccomend running n>2 on your own machine
-n = 2
+# I wouldn't reccomend running n>3 on your own machine
+n = 3
 if __name__ == '__main__':
     nodes = multiprocessed_tree(n)
+    print(str(nodes.get_total_nodes()) + " total nodes")
     #createNode(buildBaseState(n))
